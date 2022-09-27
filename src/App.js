c241d3a363f5140bc1664d8cc2a5dc3caa4326d0
Jabulani Sifundza <jjsifundza@gmail.com>
2022-09-27 11:34:33 +0200

    Loader bug fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, {useRef, useEffect, useState} from "react";
 import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as qna from "@tensorflow-models/qna";
+import {Puff} from "react-loader-spinner";
+import {Fragment} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () =>{
+	const passageRef = useRef(null);
+	const questionRef = useRef(null);
+
+	const [answer, setAnswer] = useState();
+	const [model, setModel] = useState(null);
+
+	const loadTFModel = async () =>{
+		const loadedModel = await qna.load()
+		setModel(loadedModel);
+		console.log("model has been loaded");
+	}
+
+	useEffect(()=>{
+		loadTFModel()
+	}, [])
+
+	const answerQuestion = async (e)=>{
+		if(e.which === 13 && model !== null){
+			//Enter key is 13
+			console.log("Question asked");
+			const passage =  passageRef.current.value;
+			const question = questionRef.current.value;
+
+			const answers = await model.findAswers(question, passage);
+			setAnswer(answers);
+			console.log(answers)
+		}
+	}
+
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				{}
+				{
+					model == null?
+					<div>
+						<div>Model Loading</div>
+						<Puff
+						type="Puff"
+						color="#aa0000"
+						height={100}
+						width={100}/>
+					</div>
+					: 
+					<Fragment>
+						Enter your desired Passage/Text
+						<textarea ref={passageRef} rows="30" cols="100"></textarea>
+						Ask a question about the above passage
+						<input ref={questionRef} onKeyPress={answerQuestion} size="80"></input>
+						Answers
+						{answer ? answer.map((ans, idx) => <div><b>Answer {idx+1} - </b>{ans.text} ({Math.floor(ans.score*100)/100})</div>):""}
+
+					</Fragment>
+
+				}
+			</header>
+		</div>
+	)
+
+
+
+
 }
 
 export default App;
